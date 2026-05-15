f3e5b2227a33ca9ca4274d1f89e6e81b0a50a30d
huijun1224hj-ai <huijun1224.hj@gmail.com>
2026-05-15 17:28:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SHOP = {
   whatsapp: "601121476568",
-  supabaseUrl: "https://wqhcaoojvkayhcozuheg.supabase.co/rest/v1/"
+  supabaseUrl: "https://wqhcaoojvkayhcozuheg.supabase.co"
   supabaseAnonKey: "sb_publishable_1mTK9khDS4f-b8FN0oW9og_GSzVlBGz"
 };
